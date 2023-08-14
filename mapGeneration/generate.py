e36cb731e06d5cd8acdb2e985cba56d1bb5197fc
cpreinholtz <cpr978@gmail.com>
2023-08-13 22:54:30 -0700

    wiggle waggle v1.0git add .git add .! itsssss working

--- mapGeneration/generate.py
+++ mapGeneration/generate.py
@@ -11,89 +11,225 @@ import matplotlib.pyplot as plt
 # 30 leds per meter, convert to inches per led.
 # .........................................
 
-spacing = 1/30*39.3701
-
-def calcRings(nPixels , radias , offset, fixed, xy, invert):
-  rings = []
-
-  for nPixels , radias , offset, fixed, xy, invert in zip(nPixels , radias , offset, fixed, xy, invert):  
-    ring = np.zeros([nPixels,3])
-    deltaRadians = spacing / radias
-    if invert: 
-      deltaRadians = - deltaRadians
-    #print(deltaRadians)
-    for pixel in range(nPixels):
-      angle = offset + pixel*deltaRadians
-      #print(angle)
-      if xy:
-        x = radias * np.cos(angle)
-        y = radias * np.sin(angle)
-        z = fixed
-      else:
-        x = radias * np.cos(angle)
-        y=fixed
-        z = radias * np.sin(angle)
-      ring[pixel,:] = [x,y,z]
-    rings.append(ring)
-  return rings
+spacing = 1.34 #1/30*39.3701
 
 
 
-# .........................................
-#actual values
-# .........................................
+def calcRing(radias, offset_rad, skip, full, fixed):
+  ring = []
+  deltaRadians = spacing / radias
+  for pixel in range(86):
+    angle = offset_rad + pixel*deltaRadians
+    if pixel in skip or pixel * deltaRadians > 2 * np.pi or pixel * deltaRadians < -2 * np.pi :
+      continue
+    ncos = radias * np.cos(angle)
+    nsin = radias * np.sin(angle)
+    if full:
+      ring.append([ncos,nsin,fixed])
+    else:
+      ring.append([fixed,ncos,nsin])
+  return ring
+
+def calcRings():
+  res=[]
+  bigr=18.25
+  littler=13.7
+  #############################################
+  ########## ring 0 little, left, full
+  radias = -littler # + radias for  full left
+  offset_rad = .32  * np.pi *2 + np.pi
+  skip = []
+  full = True
+  fixed = 15
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+  #############################################
+  ########## ring 1 big, left, full
+  radias = -bigr # + radias for  full left
+  offset_rad = .385  * np.pi*2 + np.pi
+  skip = []
+  full = True
+  fixed = 8
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
 
 
-radias = [27.25/2, 36.25/2, 36.25/2, 27.25/2, 27.25/2, 36.25/2, 36.25/2, 27.25/2] # radias of each ring in inches
-#Pixels = [70,80,80,70,70,80,80,70]
-nPixels = (np.floor(np.array(radias)*2*np.pi/spacing)).astype(int)
-nCirc = np.array(radias)*2*np.pi
-offset_angle = np.array([0, 0, 0 ,0, 180, 180, 180, 180])
+  #############################################
+  ########## ring 3 big, left, partial
+  radias = -bigr # + radias for  full left
+  offset_rad = .144  * np.pi*2 + np.pi
 
-offset = offset_angle * np.pi / 180
-fixed = [-15, -8, 8, 15, -15, -8, 8 ,15]
-xy = [1,1,0,0,1,1,0,0]
-invert = [1,1,1,1,0,0,0,0]
+  sz = [5,10,5,6,6,5,10,5,6,6]
+  skip = []
+  idx=0
+  for s in sz:
+    skip.append(idx+s)
+    skip.append(idx+s+1)
+    idx += s+2
+  print(skip)
 
-rings = calcRings(nPixels , radias , offset, fixed, xy, invert);
+  #skip = [5, 6, 17,18,  24,25,  32,33,   40,41, 47,48, 59,60 , 66, 67, 74, 75]
+  full = False
+  fixed = -8
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+  #############################################
+  ########## ring 3 litte, left, partial
+  radias = -littler # + radias for  full left
+  offset_rad = .09  * np.pi*2 + np.pi
+  sz = [10,8,8,10,8,8]
+  skip = []
+  idx=0
+  for s in sz:
+    skip.append(idx+s)
+    skip.append(idx+s+1)
+    idx += s+2
+  print(skip)
+  full = False
+  fixed = -15
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+
+  #############################################
+  ########## ring 4 little, right, full
+  radias = littler # + radias for  full left
+  offset_rad = .37  * np.pi *2 + np.pi
+  skip = []
+  full = True
+  fixed = -15
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+  #############################################
+  ########## ring 5 big, right, full
+  radias = bigr # + radias for  full left
+  offset_rad = .40  * np.pi*2 + np.pi
+  skip = []
+  full = True
+  fixed = -8
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+
+  #############################################
+  ########## ring 3 big, right, partial
+  radias = bigr # + radias for  full left
+  offset_rad = 0.16  * np.pi*2 + np.pi
+  sz = [6,6,5,10,5,6,6,5,10,5]
+  skip = []
+  idx=0
+  for s in sz:
+    skip.append(idx+s)
+    skip.append(idx+s+1)
+    idx += s+2
+  print(skip)
+  full = False
+  fixed = 8
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+  #############################################
+  ########## ring 3 litte, right, partial
+  radias = littler # + radias for  full left
+  offset_rad = 0.1  * np.pi*2 + np.pi
+  sz = [8,8,11,8,8,11]
+  skip = []
+  idx=0
+  for s in sz:
+    skip.append(idx+s)
+    skip.append(idx+s+1)
+    idx += s+2
+  print(skip)
+  full = False
+  fixed = 15
+  #############################################
+  res.append(calcRing(radias, offset_rad, skip, full, fixed))
+  ##############################################
+
+
+  return res
+
+
+
+
+
+
+
+
+rings = calcRings()
+#parse results HERE
+nRings=0
+nPixelsFinal = 0
+nPixelsPerRing = []
+
+for cnt,ring in enumerate(rings):
+  nRings+=1
+  nPixelsThisRing=0
+  for i in ring:
+    nPixelsThisRing+=1
+    nPixelsFinal += 1
+  nPixelsPerRing.append(nPixelsThisRing)
 
 
 # .........................................
 # print it
 # .........................................
 #print(rings)
-print("// generated via python")
-print("// total circumference inches: ", np.sum(nCirc))
-print("const int nRings = ",np.size(nPixels),"; // number of rings, use this for assigning to pins")
-print("#define NUM_LEDS ",np.sum(nPixels))
-print("//const int nMaxPixels =",np.max(nPixels))
+str = ""
 
-print("const int nPixelsPerRing [",np.size(nPixels),"] = { //number of pixels per ring, used to mapto buffers and pins later")
-delim = ""
-for cnt,ring in enumerate(rings):  
-  print(delim, nPixels[cnt])
-  delim = ","
-print("};")
+str+= "// generated via python\n"
+
+str+= "const int nRings = {}; // number of rings, use this for assigning to pins\n".format(nRings)
+str+= "#define NUM_LEDS {}\n".format(nPixelsFinal)
+str+= "//const int nMaxPixels ={}\n".format(np.max(np.array(nPixelsPerRing)))
 
 
-print("float ledMap [",np.sum(nPixels),"][3] = { // { x,y,z} pixel map")
-last = nPixels-1
+str+= "//number of pixels per ring, used to mapto buffers and pins later\n"
+str+= "const int nPixelsPerRing [{}] = ".format(nRings)+"{" 
+delim=""
+for val in nPixelsPerRing:
+  str+= delim + "{}".format(val)
+  delim=","
+str += "};\n"
+
+
+str+= "float ledMap [NUM_LEDS][3] =\n"
+str+="{"
+this =0
 for cnt,ring in enumerate(rings):
-  print("//ring: ", cnt, " nPixels this ring:", nPixels[cnt] )
+  str+="//ring: {} nPixels this ring:{}\n".format(cnt,nPixelsPerRing[cnt])
   for i in ring:
-    if cnt < last:
-      print("  {", i[0], ", ", i[1],", ", i[2],"},")
+    this = this +1
+    str+= "  {"
+    str+= "{:.4f}, {:.4f}, {:.4f}".format(i[0],i[1],i[2])
+    str+= "}"
+    if this < np.sum(nPixelsFinal):
+      str+= ",\n"
     else:
-      print("  {", i[0], ", ", i[1],", ", i[2],"}")
-print("};")
+      str+= "\n"
 
-print("//just printing constants again to prevent scrolling")
-print("// total circumference inches: ", np.sum(nCirc))
-print("//const int nRings = ",np.size(nPixels),"; // number of rings, use this for assigning to pins")
-print("//#define NUM_LEDS ",np.sum(nPixels))
-print("//const int nMaxPixels = ",np.max(nPixels))
 
-print("// end generated via python")
+str+= "};\n"
+
+
+str+= "// end generated via python\n"
+
+#print(str)
+
+with open("../examples/ANIMapping_3d/wag.h", "w") as text_file:
+    text_file.write(str)
+
+
 # .........................................
 #plot it
 # .........................................
@@ -104,12 +240,18 @@ ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 
 for cnt,ring in enumerate(rings):
-    ax.scatter(ring[:, 0], ring[:, 1], ring[:, 2])
+    ring = np.array(ring)
+    ax.scatter(ring[2:-1, 0], ring[2:-1, 1], ring[2:-1, 2], c='blue')
+
+    ax.scatter(ring[0, 0], ring[0, 1], ring[0, 2], c='green', marker='*', s=10)
+    ax.scatter(ring[1, 0], ring[1, 1], ring[1, 2], c='yellow', marker='*', s=10)
+    ax.scatter(ring[-1, 0], ring[-1, 1], ring[-1, 2], c='red', marker='*', s=10)
+
     #print(cnt)
 ax.set_aspect('equal')
 
 
-#plt.show()
+plt.show()
 
 
 
